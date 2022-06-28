c8173df8296d6e717a562fae0a4958850c40d4c2
umaumayuma <220c0240@s.obirin.ac.jp>
2022-06-28 18:09:59 +0900

    3日表示

--- main.js
+++ main.js
@@ -7,22 +7,14 @@ fetch(api)
 }
 
 function makePage(data) {
-    setData('day0',dateFormat(data.daily.time[0]));
-    setData('day1',dateFormat(data.daily.time[1]));
-
-    setData('weathercode0',getWMO(data.daily.weathercode[0]));
-    setData('weathercode1',getWMO(data.daily.weathercode[1]));
-
-  
-    setData('temperature_2m_max0',data.daily.temperature_2m_max[0] + '°C');
-    setData('temperature_2m_max1',data.daily.temperature_2m_max[1] + '°C');
-  
-    setData('temperature_2m_min0',data.daily.temperature_2m_min[0] + '°C');
-    setData('temperature_2m_min1',data.daily.temperature_2m_min[1] + '°C');
-  
-    setData('precipitation_sum0',data.daily.precipitation_sum[0] + 'mm');
-    setData('precipitation_sum1',data.daily.precipitation_sum[1] + 'mm');
-
+    for(let i=0;i<3;i++) {
+      setData('day'+i,dateFormat(data.daily.time[i]));
+      setData('weathercode'+i,getWMO(data.daily.weathercode[i]));
+      setData('temperature_2m_max'+i,data.daily.temperature_2m_max[i] + '℃');
+      setData('temperature_2m_min'+i,data.daily.temperature_2m_min[i] + '℃');
+      setData('precipitation_sum'+i,data.daily.precipitation_sum[i] + 'mm');
+    }
+   
     if ( data.daily.precipitation_sum[0] > 0 ) {
         document.getElementById('body').style.backgroundImage
         = 'linear-gradient(#00ffff 5%, #fff 10%)';   
