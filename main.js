de88258334a8299f0697693548f3f942127cc0e7
umaumayuma <220c0240@s.obirin.ac.jp>
2022-07-01 15:44:32 +0900

    chabge to Hokkaido

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const api = 'https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=Asia%2FTokyo';
+const api = 'https://api.open-meteo.com/v1/forecast?latitude=43.06417&longitude=141.34694&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=Asia%2FTokyo';
 
 function getData() {
 fetch(api)
